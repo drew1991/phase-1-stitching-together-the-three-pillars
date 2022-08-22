0f08a8ade09012648dc6007f3a36280656549764
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-08-22 10:05:25 +0300

    completed assignment

--- demo.js
+++ demo.js
@@ -1,20 +1,18 @@
 // We use the Objects below to control toggling like / unlike status
-
 const glyphStates = {
   "♡": "♥",
   "♥": "♡"
 };
-
 const colorStates = {
   "red" : "",
   "": "red"
 };
-
 // STEP 1: The line of code below is what lets JavaScript find the elements that
 // we want to make clickable. Without JavaScript, clicking on these heart shapes
 // does nothing. Uncomment the code and refresh the demo page. 
 
 // const articleHearts = document.querySelectorAll(".like-glyph");
+const articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   const heart = e.target;
@@ -28,12 +26,14 @@ function likeCallback(e) {
       // alert(serverMessage);
       // heart.innerText = glyphStates[heart.innerText];
       // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
     });
 }
-
 // STEP 3: In order for the call to the server and the update of the screen to
 // work, we need to add a click event listener to the elements we identified in
 // STEP 1. That's Pillar 2, event handling. Uncomment this code:
@@ -41,6 +41,9 @@ function likeCallback(e) {
 // for (const glyph of articleHearts) {
 //   glyph.addEventListener("click", likeCallback);
 // }
+for (const glyph of articleHearts) {
+glyph.addEventListener("click", likeCallback);
+}
 
 // STEP 4: 
 
@@ -48,11 +51,9 @@ function likeCallback(e) {
 // working demonstration of our reference example. Sure, it's maybe not as
 // pretty as a professional site, but they're only different from our site in
 // style, not substance.
-
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
-
 function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
